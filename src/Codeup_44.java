ae0e9546fa939f6d3a79a0bf16e6979932b80331
ChoiJiMin <chlwlals8708@gmail.com>
2022-10-04 09:36:41 +0900

    .

---
+++ src/Codeup_44.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Codeup_44 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long a = Integer.parseInt(br.readLine());
+
+        System.out.println(++a);
+    }
+}
