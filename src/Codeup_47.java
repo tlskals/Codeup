16c66eaa46027f92a5a6c00566527403d59963d3
ChoiJiMin <chlwlals8708@gmail.com>
2022-10-12 00:19:43 +0900

    22.10.12

---
+++ src/Codeup_47.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Codeup_47 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        br.close();
+
+        bw.write(String.valueOf(a<<1));
+        bw.flush();
+        bw.close();
+
+
+    }
+}
